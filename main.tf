4fae5972a7fc2b22b7e02c31b6d66d516e66c86c
Nadhirovich <mnadir20@gmail.com>
2021-05-22 09:59:35 +0100

    resolve Bucket duplicate name isue

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-bucket = "terraform-up-and-running-state"
+resource "aws_s3_bucket" "Tbessa-nadhirovich-bucket" {
+bucket = "my-test-buacket"
 # Prevent accidental deletion of this S3 bucket
 lifecycle {
 prevent_destroy = true
